a46f54a5281b7669ac2fac21762a68c33c432662
Marcell <havlikmarcell@gmail.com>
2024-10-29 23:52:59 +0100

    detached head stuff half work

--- src/stateful_transformation/GitTracker.jl
+++ src/stateful_transformation/GitTracker.jl
@@ -12,25 +12,21 @@ LLM_commit_changes(TODO) = LLM_overview(TODO, max_token=29, extra="We need a ver
 # TODoO = "a\ra isay hi"
 # new_name = LLM_job_to_do3(TODoO)
 
-@kwdef mutable struct Branch
+@kwdef mutable struct WorkTree
 	initial_hash::LibGit2.GitHash
 	original_repo::LibGit2.GitRepo
 	repo::LibGit2.GitRepo
 	worktreepath::String=""
-	branch::String=""                           # max 6 token thing
-	# branch_counter::Int=0
 end
 @kwdef mutable struct GitTracker
-	tracked_gits::Vector{Branch}
+	tracked_gits::Vector{WorkTree}
 	todo::String=""
 end
 
 GitTracker!(ws, p::PersistableState, conv) = begin
 	# clean_unnamed()
-  branch = "$(INITIAL_BRANCH_NAME)-$(string(floor(Int,datetime2unix(now())))[5:end])"
-	
   # TODO if there is branch name collision then regerenrate extending the list what we 'don't want' 
-	gits = Branch[]
+	gits = WorkTree[]
 	for (i, project_path) in enumerate(ws.project_paths)
 		expanded_project_path = expanduser(abspath(expanduser(project_path)))
 		# @show expanded_project_path
@@ -42,26 +38,24 @@ GitTracker!(ws, p::PersistableState, conv) = begin
 		ws.project_paths[i]=worktreepath
 
 		original_repo = LibGit2.GitRepo(expanded_project_path)
-		create_worktree(original_repo, branch, worktreepath)
-		orig_branch = LibGit2.head_oid(original_repo)
-		# @show orig_branch
+		create_worktree(original_repo, worktreepath)
+		init_hash = LibGit2.head_oid(original_repo)
 		# @show worktreepath
 		# @show LibGit2.GitRepo(worktreepath)
-		push!(gits, Branch(orig_branch, original_repo, LibGit2.GitRepo(worktreepath), worktreepath, branch))
+		push!(gits, WorkTree(init_hash, original_repo, LibGit2.GitRepo(worktreepath), worktreepath))
 	end
 	ws.root_path, ws.rel_project_paths = resolve(ws.resolution_method, ws.project_paths)
 	conv_path = abs_conversaion_path(p, conv)
 
 	init_git(conv_path)
 	conv_repo = LibGit2.GitRepo(conv_path)
-	push!(gits, Branch(LibGit2.head_oid(conv_repo), conv_repo, conv_repo, conv_path, "master"))
+	push!(gits, WorkTree(LibGit2.head_oid(conv_repo), conv_repo, conv_repo, conv_path, "master"))
 	GitTracker(gits,"")
 end
 
 is_git(path)    = isdir(joinpath(path, ".git"))
-orig_branch(br) = LibGit2.shortname(br.initial_hash)
 commit_changes(g::GitTracker, message::String) = begin
-	rename_branch(g, message)
+	isempty(g.todo) && (g.todo = message)  # Initialize the MAIN todo!
 	commit_msg = LLM_commit_changes(message)
 	# @show commit_msg
 	for git in g.tracked_gits
@@ -91,43 +85,27 @@ init_git(path::String, forcegit = true) = begin
 		@assert false "yeah this end is unsupported now :D.. TODO"
 		return nothing
 	end
-end 
-rename_branch(g::GitTracker, TODO) = begin
-	new_name = ""
-	# @show new_name
-	# println(join([LibGit2.workdir(git.repo) for git in g.tracked_gits], "\n"))
-	for git in g.tracked_gits
-		!startswith(git.branch, INITIAL_BRANCH_NAME) && continue
-		git.branch in ["master", "main"] && continue
-		isempty(new_name) && ((new_name = LLM_branch_name(TODO)); (g.todo=TODO))
 	
-		# @show LibGit2.workdir(git.original_repo)
-		cd(LibGit2.workdir(git.original_repo)) do
-			run(`git branch -m "$(git.branch)" "$(new_name)"`)
-		end
-		git.branch = new_name
-	end
-end
+end 
 
 
-create_worktree(original_repo, branch_name, worktree_path) = begin
-	LibGit2.create_branch(original_repo, branch_name, LibGit2.GitCommit(original_repo, LibGit2.head_oid(original_repo)))
+create_worktree(original_repo, worktree_path) = begin
 	# @show worktree_path
-	# @show branch_name
-	# @show "initializing"
 	cd(LibGit2.workdir(original_repo)) do
-		run(`git worktree add $worktree_path $branch_name`)
+		run(`git worktree add -d $worktree_path`)
 	end
 	# @show "successfully"
 end
 
-merge_git(g::GitTracker) = for branch in g.tracked_gits
-	merge_git(branch, g.todo)
+merge_git(g::GitTracker) = for worktree in g.tracked_gits
+	merge_git(worktree, g.todo)
 end
-merge_git(b::Branch, commit_msg::String)                         = merge_git(b.original_repo, b.branch, commit_msg)
+merge_git(w::WorkTree, commit_msg::String)                         = merge_git(w.original_repo, LLM_branch_name(commit_msg), commit_msg)
 merge_git(repo::GitRepo, branch::String, commit_msg::String)     = merge_git(LibGit2.workdir(repo),branch,commit_msg)
 merge_git(repo_path::String, branch::String, commit_msg::String) = begin
 	cd(repo_path) do
+		run(`git checkout -b $(branch)`) # no name collision please while loop should be here...
+
 		stash_result = read(`git stash save "Temp stash before $(branch)"`, String)
 		has_stash = !occursin("No local changes to save", stash_result)
 		merge_result = read(`git merge $(branch) --no-ff -m "$(commit_msg)"`, String)
