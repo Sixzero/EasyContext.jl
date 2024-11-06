d5887efe3b9889de21cbaedb6a9466a23f9d4908
Cvikli <havlikmarcell@gmail.com>
2024-11-06 14:09:41 +0100

    Improved JSON formatting in conversation saving

--- src/file_io/Conversation_JSON.jl
+++ src/file_io/Conversation_JSON.jl
@@ -1,9 +1,13 @@
 using JSON3
 
 JSON3.StructTypes.StructType(::Type{WebMessage})      = JSON3.StructTypes.Struct()
-# JSON3.StructTypes.StructType(::Type{ConversationX})   = JSON3.StructTypes.Struct()
 JSON3.StructTypes.StructType(::Type{<:ConversationX}) = JSON3.StructTypes.Mutable()
 
-save_conversation(filepath::String, conv::ConversationX) = write(filepath, JSON3.write(conv))
+save_conversation(filepath::String, conv::ConversationX) = write(filepath, json_pretty(conv) * "\n")
 load_conversation(filepath::String)                      = JSON3.read(read(filepath, String), ConversationX{WebMessage})
+function json_pretty(obj)
+    buf = IOBuffer()
+    JSON3.pretty(buf, obj)
+    String(take!(buf))
+end
 
