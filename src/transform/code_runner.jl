be5e2b9992ce1248d2edb5c64bb795da2471523a
Marcell <havlikmarcell@gmail.com>
2024-11-15 01:11:54 +0100

    Workspace root path handling improved, asynchronous context added to codeblock runner

--- src/transform/code_runner.jl
+++ src/transform/code_runner.jl
@@ -1,22 +1,20 @@
 
-codeblock_runner(extractor::CodeBlockExtractor; no_confirm=false) = begin
-    return !extractor.skip_code_execution ? execute_shell_commands(extractor; no_confirm) : OrderedDict{String, CodeBlock}()
-end
+using Base.Threads
 
-function execute_shell_commands(extractor::CodeBlockExtractor; no_confirm=false)
+codeblock_runner(extractor::CodeBlockExtractor; no_confirm=false, async=false) = !extractor.skip_code_execution ? execute_shell_commands(extractor; no_confirm, async) : OrderedDict{String, CodeBlock}()
+
+function execute_shell_commands(extractor::CodeBlockExtractor; no_confirm=false, async=false)
     for (command, task) in extractor.shell_scripts
         cb = fetch(task)
         if cb !== nothing
             extractor.shell_results[command] = cb
-            output = execute_code_block(cb; no_confirm)
-            push!(extractor.shell_results[command].run_results, output)
-            # !isempty(strip(output)) && println("\e[36mOutput:\e[0m $output")
-            # println("\n\e[36mCommand:\e[0m $command")
+            push!(extractor.shell_results[command].run_results, execute_code_block(cb; no_confirm))
         else
-            @warn "TODO We couldn't run the block, there might have been an error." # we think the error is because @async_showerr crashed and returned nothing.
+            @warn "TODO We couldn't run the block, there might have been an error."
             println("\e[31mWarning:\e[0m Task for command '$command' returned nothing")
         end
     end
+    (async) && (println(); dialog())  # Show dialog arrow in async context or when explicitly set
     return extractor.shell_results
 end
 
