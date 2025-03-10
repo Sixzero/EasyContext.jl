802f69cc2bc0ab6ee119a6e17dbddda529343d90
Cvikli <havlikmarcell@gmail.com>
2025-03-10 17:21:19 +0100

    Enhanced user instruction clarity

--- src/prompts/guide_highlight_changes.jl
+++ src/prompts/guide_highlight_changes.jl
@@ -5,5 +5,5 @@ const highlight_changes_guide_v2 = """
 Always use the "... existing code ..." if you skip or leave out codepart!
 If you don't want to skip some part of code then just use the "// ... existing code ...".
 
-Please focus on the changes you want to make and write only that and abbreviate the rest. The changes will be applied to the codebase by the user.
+Please focus on the changes you want to make and write only that and abbreviate the rest. The changes will be applied to the codebase by the user, that's why you have to focuse on the changes only.
 """ 
